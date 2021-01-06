6912e272b4bb45d25d13dcce5b01f55d53549b37
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2021-01-06 13:04:07 +0100

    Update Admin.java

--- src/main/java/de/joemiagroup/krawumm/Admin.java
+++ src/main/java/de/joemiagroup/krawumm/Admin.java
@@ -1,7 +1,30 @@
 package de.joemiagroup.krawumm;
 
 public class Admin extends RegisteredUser {
-    public void deleteUser() {}
-    public void releaseExperiment() {}
-    public void deleteForm() {}
+
+    public Admin(String _name, String _email, String _password) {
+        this.username           = _name;
+        this.email              = _email;
+        this.password           = _password;
+        this.iscreator          = false;
+        this.uploadedmaterial   = new String[]{}; //Don't know about the new String[]. intelliJ asked for it.
+        this.bookmarks          = new String[]{};
+    }
+
+    public void deleteUser(RegisteredUser _user) {
+        _user = null; //apparently gets delete by garbage collector.
+    }
+
+    public void releaseExperiment(String _name, Form _data) {
+        /*
+        Constructor needed. Also this doesn't work anyway.
+        Experiment _name = new Experiment(_data);
+        */
+
+        deleteForm(_data);
+    }
+
+    public void deleteForm(Form _form) {
+        _form = null; //apparently gets delete by garbage collector.
+    }
 }
