fd91e6f684c20a48661f912cc2c0d8dc37d784c2
peterhaferkeks <peters-emil@t-online.de>
2021-01-11 08:36:59 +0100

    Erstellen der Klasse Comment

---
+++ src/main/java/de/joemiagroup/krawumm/Comment.java
@@ -0,0 +1,55 @@
+package de.joemiagroup.krawumm;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+
+
+
+    // attributes
+    private String text;
+    private String[] pictures;
+    private RegisteredUser commentator;
+    private LocalDateTime date;
+
+    // functions
+
+    // getter + setter text
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        if (text.length() <= 1000) {
+            this.text = text;
+        }
+        // TODO: Fehlermeldung
+    }
+
+    // getter + setter pictures
+    public String[] getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(String[] pictures) {
+        this.pictures = pictures;
+    }
+
+    // getter + setter commentator
+    public RegisteredUser getCommentator() {
+        return commentator;
+    }
+
+    public void setCommentator(RegisteredUser commentator) {
+        this.commentator = commentator;
+    }
+
+    // getter + setter date
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
